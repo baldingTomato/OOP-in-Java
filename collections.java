c64c12000b904e321e19222fb7725840d331fbf9
baldingTomato <86657058+baldingTomato@users.noreply.github.com>
2021-12-02 11:03:58 +0100

    Created new project for lab
    It's a project about different collections in Java.

---
+++ collections.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.util.LinkedList;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        int choice = 0;
+        Scanner sc = new Scanner(System.in);
+        String input = new String();
+        String newItem = new String();
+        String takenItem = new String();
+
+        /*code for Stack
+        Stack<String> bag = new Stack<String>();
+
+        while(choice == 0){
+
+            System.out.println("What do you want to do?");
+            input = sc.nextLine();
+
+            switch(input){
+
+                case "add":
+                    System.out.println("What kind of item do you want to add?");
+                    newItem = sc.nextLine();
+                    bag.add(newItem);
+                    break;
+                case "take":
+                    System.out.println("Taken item is " + bag.pop());
+                    break;
+                case "print":
+                    System.out.println("Your stack looks like this: " + bag);
+                    break;
+                case "exit":
+                    choice++;
+                    break;
+                default:
+                    System.out.println("Sorry, I don't understand!");
+                    break;
+            }
+
+        }
+        */
